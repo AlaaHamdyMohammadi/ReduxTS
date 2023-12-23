a3e5138cc041f854d9ce5c29992150c06c68d64c
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-12-23 20:50:40 +0200

    Different Interfaces

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -4,20 +4,28 @@ interface RepositoriesState {
   data: string[];
 }
 
-interface SearchRepositoriesAction{
-    type: "search_repositories";
+interface SearchRepositoriesAction {
+  type: "search_repositories";
 }
 interface SearchRepositoriesSuccessAction {
   type: "search_repositories_success";
   payload: string[];
 }
 interface SearchRepositoriesErrorAction {
-    type: "search_repositories_error";
-    payload: string;
+  type: "search_repositories_error";
+  payload: string;
 }
 
-const reducer = (state: RepositoriesState, action: any): RepositoriesState => {
+const reducer = (
+  state: RepositoriesState,
+  action:
+    | SearchRepositoriesAction
+    | SearchRepositoriesSuccessAction
+    | SearchRepositoriesErrorAction
+): RepositoriesState => {
   //Must determine the return of reducerFunc to prevent any weird data, so just the types of data into the interface
+  
+  
   switch (action.type) {
     case "search_repositories": //Guest seach button every thing is empty
       return { loading: true, error: null, data: [] };
